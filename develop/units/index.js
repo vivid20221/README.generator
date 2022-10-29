b61bc7d392342b42cbfaddd9eaf22b701bc8af5c
vivid20221 <julainj9011@gmail.com>
2022-10-28 20:24:32 -0500

    added npm

---
+++ develop/units/index.js
@@ -0,0 +1,23 @@
+// TODO: Include packages needed for this application
+const packages = require('')
+
+
+// TODO: Create an array of questions for user input
+const questions = {
+    queue: [ 
+        "What was your motivation?",
+         "Why did you build this project?", 
+        "What problem does it solve?",
+        "What did you learn?",
+        "What makes your project stand out?"],
+
+// TODO: Create a function to write README file
+   writeToFile(fileName, data) {
+    this.queue.pop();
+},
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
